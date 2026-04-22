d1321a93b9c96e399a18781555eb39776aa92a70
Dawood <dawoodsoomro56@gmail.com>
2026-04-22 22:45:32 +0500

    feat: add process forking and command execution with path searching

--- Processes-shell/wish.c
+++ Processes-shell/wish.c
@@ -2,21 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+char **shell_path;
+int path_count = 1;
 
 void print_error() {
     char error_message[30] = "An error has occurred\n";
     write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
+char** tokenize(char *cmd) {
+    int pos = 0;
+    char **tokens = malloc(64 * sizeof(char*));
+    char *token;
+    while ((token = strsep(&cmd, " \t\n")) != NULL) {
+        if (strlen(token) == 0) continue;
+        tokens[pos++] = token;
+    }
+    tokens[pos] = NULL;
+    return tokens;
+}
+
 int main(int argc, char *argv[]) {
-    if (argc > 2) { print_error(); exit(1); }
-    char *line = NULL;
-    size_t len = 0;
+    shell_path = malloc(sizeof(char*));
+    shell_path[0] = strdup("/bin");
+
+    char *line = NULL; size_t len = 0;
     while (1) {
         printf("wish> ");
         if (getline(&line, &len, stdin) == -1) break;
-        if (strcmp(line, "exit\n") == 0) exit(0);
+
+        char **args = tokenize(line);
+        if (args[0] == NULL) { free(args); continue; }
+        if (strcmp(args[0], "exit") == 0) exit(0);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            char p[512]; snprintf(p, 512, "%s/%s", shell_path[0], args[0]);
+            execv(p, args);
+            print_error(); exit(1);
+        } else { wait(NULL); }
+        free(args);
     }
-    free(line);
     return 0;
 }
