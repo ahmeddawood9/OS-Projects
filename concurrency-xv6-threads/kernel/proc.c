2f9cf320e386219c9a82f6d80544733566fc097d
Dawood <dawoodsoomro56@gmail.com>
2026-05-04 23:18:37 +0500

    feat: implement join() system call in kernel/proc.c

--- concurrency-xv6-threads/kernel/proc.c
+++ concurrency-xv6-threads/kernel/proc.c
@@ -61,3 +61,44 @@ clone(void(*fcn)(void *, void *), void *arg1, void *arg2, void *stack)
 
   return pid;
 }
+
+int
+join(void **stack)
+{
+  struct proc *p;
+  int havekids, pid;
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for zombie children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != proc || p->pgdir != proc->pgdir)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+        pid = p->pid;
+        *stack = p->stack; // Assuming we store stack in struct proc for join
+        kfree(p->kstack);
+        p->kstack = 0;
+        p->state = UNUSED;
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || proc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(proc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
